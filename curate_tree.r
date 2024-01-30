91f10098c39c14352527affee42e67757dc792a1
KLab-UT <utt.klab@gmail.com>
2024-01-30 13:28:13 -0700

    added meeting notes

--- curate_tree.r
+++ curate_tree.r
@@ -1,6 +1,9 @@
 library(ape)
 library(phytools)
 
-fr10_tree <- read.tree('test.tre')
+fr10_tree <- read.tree('U44831_in_8342_fixed.fasta.contree')
+fr10_tree_rooted <- root(fr10_tree, outgroup = "Pipa_carvalhoi")
+nodelabels(fr10_tree_rooted$node.label,node=2:fr10_tree_rooted$Nnode+Ntip(fr10_tree_rooted),
+    adj=c(1,-0.2),frame="none")
 
-plotTree(fr10_tree)
+plotTree(fr10_tree_rooted, show.node.label = TRUE)
