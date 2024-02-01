32bd98d352ad31bd05acb8e147ceb87b43e9c8c4
KLab-UT <utt.klab@gmail.com>
2024-02-01 13:31:56 -0700

    added script called get_phylo_dist.r that creates a csv file 'see tmp file called test.csv)

--- plots_and_figures/fr10_apo_phylodist/get_phylo_dist.r
+++ plots_and_figures/fr10_apo_phylodist/get_phylo_dist.r
@@ -11,8 +11,11 @@ plot(apoAII_tree)
 
 # get pairwise phylogenetic distance
 pairwise_dist <- distTips(apoAII_tree, tips='all', 'patristic')
+pd_matrix <- as.matrix(pairwise_dist)
 
-
+# write distances to csv file
+pairwise_dist_df <- as.data.frame(pd_matrix)
+write.csv(pairwise_dist_df, "test.csv", row.names = TRUE)
 
 
 
