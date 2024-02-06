bcad6b7d1e904e775105bb87de0a275a2b48965d
reaganman <reaganman@github.com>
2024-02-06 14:25:21 -0700

    update plots and scripts

---
+++ plots/make_apo_plots.sh
@@ -0,0 +1,57 @@
+#Reload trees 
+git checkout HEAD -- ../apo-fr10_alignments/
+
+fr10_tree_files=(
+  "../apo-fr10_alignments/ApoA-II_aligned.plottree"
+  "../apo-fr10_alignments/ApoA-V_aligned.plottree"
+  "../apo-fr10_alignments/ApoC-IV_aligned.plottree"
+  "../apo-fr10_alignments/ApoA-IV_aligned.plottree"
+  "../apo-fr10_alignments/ApoC-III_aligned.plottree"
+  "../apo-fr10_alignments/ApoC-I_aligned.plottree"
+  "../apo-fr10_alignments/ApoA-I_aligned.plottree"
+  "../apo-fr10_alignments/ApoC-II_aligned.plottree"
+  "../apo-fr10_alignments/АроЕ_aligned.plottree"
+)
+
+drp10_tree_files=(
+"../apo-drp10_alignments/ApoA-II_aligned.plottree"
+"../apo-drp10_alignments/ApoA-V_aligned.plottree"
+"../apo-drp10_alignments/ApoC-IV_aligned.plottree"
+"../apo-drp10_alignments/ApoA-IV_aligned.plottree"
+"../apo-drp10_alignments/ApoC-III_aligned.plottree"
+"../apo-drp10_alignments/ApoC-I_aligned.plottree"
+"../apo-drp10_alignments/ApoA-I_aligned.plottree"
+"../apo-drp10_alignments/ApoC-II_aligned.plottree"
+"../apo-drp10_alignments/АроЕ_aligned.plottree"
+)
+
+
+# Setup output files
+header="Homo_sapiens,Pan_troglodytes,gorilla_gorilla,Pongo_abelii,Macaca_mulatta,Papio_anubis,Mus_musculus,Rattus_norvegicus,Oryctolagus_cuniculus,Bos_taurus,Ovis_aries,Capra_hircus,Sus_scrofa,Equus_caballus,Felis_catus,lupus_familiaris,Monodelphis_domestica,Ornithorhynchus_anatini,Gallus_gallus,Meleagris_gallopavo,Anolis_carolinensis,Chrysemys_picta,Takifugu_rubripes,Oryzias_latipes,Danio_rerio,Lepisosteus_oculatus,Latimeria_chalumnae,Rhincodon_typus,Callorhinchus_milii"
+echo $header > normalized_distances_fr10.csv
+echo $header > normalized_distances_drp10.csv
+
+
+# Call R script to calculate and append normalized distances for each treefile and append the distances for that tree to the normalized_distances output"
+for tree_file in "${fr10_tree_files[@]}"; do
+  echo "$tree_file"
+  Rscript plot_phylo_dist.r "$tree_file" "$normalized_distances_fr10" Lithobates_sylvaticus Xenopus_tropicalis;
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- plots/plot_phylo_dist.r
+++ plots/plot_phylo_dist.r
@@ -1,23 +1,12 @@
 library(ape)
 library(adephylo)
 
-#treefile <- as.character(commandArgs(trailingOnly = TRUE)[1])
-#distmatrix_file <- as.character(commandArgs(trailingOnly = TRUE)[2])
-#query_species <- as.character(commandArgs(trailingOnly = TRUE)[3])
-#sister_species <- as.character(commandArgs(trailingOnly = TRUE)[4])
-
-
-distmatrix_file <- "apo-fr10_alignments/ApoA-I_aligned_distmatrix.csv"
-query_species <- "Lithobates_sylvaticus"
-sister_species <- "Xenopus_tropicalis"
-query_gene <-"ApoA-I"
-
-
-normalize_distances <- function(tree_file, query_species, sister_species){# load tree
+normalize_distances <- function(tree_file, normalized_distances_fout, query_species, sister_species){# load tree
+  # Load input and set up variables
   tree <- read.tree(tree_file)
+  fout <- read.csv(normalized_distances_fout)
   gene_name <- sub("_aligned.plottree", "", basename(tree_file))
   distmatrix_file <- sub(".treefile", "_distmatrix.csv", tree_file)
-  print(tree_file)
   
   # get pairwise phylogenetic distance
   pairwise_dist <- distTips(tree, tips='all', 'patristic')
@@ -30,52 +19,64 @@ normalize_distances <- function(tree_file, query_species, sister_species){# load
   # read distmatrix
   distmatrix <- read.csv(distmatrix_file)
   
-  # get distance between query species and sister species 
-  qs_dist <- distmatrix[distmatrix$X == query_species, sister_species]
-  
-  # get the distances between the query and all other species not including query or sister
-  q_distances <- distmatrix[distmatrix$X != query_species & distmatrix$X != sister_species, c("X", query_species), drop = FALSE]
-  
-  # Change the "X" column to row names
-  rownames(q_distances) <- q_distances$X
-  
-  # Remove the "X" column
-  q_distances <- q_distances[, -1, drop=FALSE]
-  
-  ##TODO: normalize q_distances
-  normalized_distances <- q_distances / qs_dist 
-  
-  # Set row names for the normalized_distances dataframe
-  rownames(normalized_distances) <- rownames(q_distances)
-  
-  # Transpose the dataframe
-  normalized_distances <- t(normalized_distances)
-  
-  # Update row name
-  rownames(normalized_distances)[rownames(normalized_distances) == query_species] <- query_gene
-  return(normalized_distances)
+  # Check if sister_species is a column in distmatrix
+  if (sister_species %in% colnames(distmatrix)) {
+    # get distance between query species and sister species 
+    qs_dist <- distmatrix[distmatrix$X == query_species, sister_species]
+    
+    # get the distances between the query and all other species not including query or sister
+    q_distances <- distmatrix[distmatrix$X != query_species & distmatrix$X != sister_species, c("X", query_species), drop = FALSE]
+    
+    # Change the "X" column to row names
+    rownames(q_distances) <- q_distances$X
+    
+    # Remove the "X" column
+    q_distances <- q_distances[, -1, drop=FALSE]
+    
+    ## Normalize q_distances
+    normalized_gene_distances <- q_distances / qs_dist 
+    
+    # Set row names for the normalized_distances dataframe
+    rownames(normalized_gene_distances) <- rownames(q_distances)
+    
+    # Transpose the dataframe
+    normalized_gene_distances <- t(normalized_gene_distances)
+    
+    # Update row name
+    rownames(normalized_gene_distances)[rownames(normalized_gene_distances) == query_species] <- query_gene
+    
+    # Append normalized row to fout
+    fout <- rbind(fout, normalized_gene_distances)
+    
+    # Identify columns in fout that are not in normalized_gene_distances
+    missing_cols <- setdiff(colnames(fout), colnames(normalized_gene_distances))
+    
+    # Set values in the normalized row to NA for missing columns
+    fout[nrow(fout), missing_cols] <- NA
+    
+    # Save the updated normalized_distances_fout
+    write.csv(fout, normalized_distances_fout, row.names = TRUE)
+    
+  } 
+  else {
+    cat("sister_species is not a column in distmatrix. Skipping the normalization process.\n")
+  }
 }
 
-tree_file <- "apo-fr10_alignments/ApoC-II_aligned.plottree"
-query_species <- "Lithobates_sylvaticus"
-sister_species <- "Xenopus_tropicalis"
-
-result <- normalize_distances(tree_file, query_species, sister_species)
 
 
+treefile <- as.character(commandArgs(trailingOnly = TRUE)[1])
+normalized_distances_fout <- as.character(commandArgs(trailingOnly = TRUE)[2])
+query_species <- as.character(commandArgs(trailingOnly = TRUE)[3])
+sister_species <- as.character(commandArgs(trailingOnly = TRUE)[4])
+
+#tree_file <- "apo-fr10_alignments/ApoC-II_aligned.plottree"
+#normalized_distances_fout <- "plots/normalized_distances_fr10.csv"
+#query_species <- "Lithobates_sylvaticus"
+#sister_species <- "Xenopus_tropicalis"
+setwd("/home/reagan/bioinformatics/fr10_evolution/plots")
+
+normalize_distances(treefile, normalized_distances_fout, query_species, sister_species)
 
 
-tree_files <- c("apo-fr10_alignments/ApoA-II_aligned.plottree", "apo-fr10_alignments/ApoA-V_aligned.plottree", "apo-fr10_alignments/ApoC-IV_aligned.plottree",
-                "apo-fr10_alignments/ApoA-IV_aligned.plottree", "apo-fr10_alignments/ApoC-III_aligned.plottree", "apo-fr10_alignments/ApoC-I_aligned.plottree",
-                "apo-fr10_alignments/ApoA-I_aligned.plottree", "apo-fr10_alignments/ApoC-II_aligned.plottree", "apo-fr10_alignments/АроЕ_aligned.plottree")
-query_species <- "Lithobates_sylvaticus"
-sister_species <- "Xenopus_tropicalis"
 
-# Initialize an empty dataframe with column names
-result_df <- data.frame()
-
-# Loop through each pair and append to the result_df
-for (i in seq_along(tree_files)) {
-  pair_result <- normalize_distances(tree_files[i], query_species, sister_species)
-  result_df <- rbind(result_df, pair_result)
-}
