05f394632e766d1fd40c03248347c9b9132fb039
KLab-UT <utt.klab@gmail.com>
2024-02-08 21:50:26 -0700

    updated distance_anova.r , the file now contains the results of the anovas for both fr10 and drp10

--- plots/distance_anova.r
+++ plots/distance_anova.r
@@ -49,3 +49,47 @@ summary(results)
 # Multiple R-squared:  0.649,	Adjusted R-squared:  0.6343
 # F-statistic: 44.12 on 7 and 167 DF,  p-value: < 2.2e-16
 
+dat <- read.csv('normalized_distances_drp10.csv')
+
+dat_melted <- reshape2::melt(dat, id.vars = "Gene")
+
+head(dat_melted)
+
+results <- lm(value~relevel(factor(Gene),ref="ApoA-II"), data=dat_melted)
+
+summary(results)
+
+# Call:
+# lm(formula = value ~ relevel(factor(Gene), ref = "ApoA-II"),
+#     data = dat_melted)
+# 
+# Residuals:
+#      Min       1Q   Median       3Q      Max
+# -0.98289 -0.07954 -0.01764  0.04673  0.92255
+# 
+# Coefficients:
+#                                                Estimate Std. Error t value
+# (Intercept)                                     4.01493    0.03596  111.64
+# relevel(factor(Gene), ref = "ApoA-II")ApoA-I   -2.92468    0.04845  -60.37
+# relevel(factor(Gene), ref = "ApoA-II")ApoA-IV  -2.93757    0.04979  -59.00
+# relevel(factor(Gene), ref = "ApoA-II")ApoA-V   -3.19464    0.05211  -61.30
+# relevel(factor(Gene), ref = "ApoA-II")ApoC-I   -3.21321    0.05447  -58.99
+# relevel(factor(Gene), ref = "ApoA-II")ApoC-II  -2.51727    0.05211  -48.30
+# relevel(factor(Gene), ref = "ApoA-II")ApoC-III -2.51727    0.05211  -48.30
+# relevel(factor(Gene), ref = "ApoA-II")АроЕ     -3.14856    0.04931  -63.85
+#                                                Pr(>|t|)
+# (Intercept)                                      <2e-16 ***
+# relevel(factor(Gene), ref = "ApoA-II")ApoA-I     <2e-16 ***
+# relevel(factor(Gene), ref = "ApoA-II")ApoA-IV    <2e-16 ***
+# relevel(factor(Gene), ref = "ApoA-II")ApoA-V     <2e-16 ***
+# relevel(factor(Gene), ref = "ApoA-II")ApoC-I     <2e-16 ***
+# relevel(factor(Gene), ref = "ApoA-II")ApoC-II    <2e-16 ***
+# relevel(factor(Gene), ref = "ApoA-II")ApoC-III   <2e-16 ***
+# relevel(factor(Gene), ref = "ApoA-II")АроЕ       <2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 0.1687 on 167 degrees of freedom
+#   (57 observations deleted due to missingness)
+# Multiple R-squared:  0.9736,	Adjusted R-squared:  0.9725
+# F-statistic: 879.8 on 7 and 167 DF,  p-value: < 2.2e-16
