fa82b63aa575ffde9d9e825410b33c14873a49bf
reaganman <reaganman@github.com>
2024-02-13 21:59:21 -0700

    update plots with query_coverage.pdf and plot_query_coverage.r

--- plots/plot_query_coverage.R
+++ plots/plot_query_coverage.R
@@ -1,36 +1,45 @@
+library(ggplot2)
+library(tidyr)
+library(ggtree)
+library(dplyr)
+
 # Read the data from CSV files
 drp10_data <- read.csv("hsp_logs/drp10_hsp_log_combined.csv", header = FALSE, col.names = c("ID", "Species", "Start", "End"))
 fr10_data <- read.csv("hsp_logs/fr10_hsp_log_combined.csv", header = FALSE, col.names = c("ID", "Species", "Start", "End"))
 
 # Combine both datasets
 combined_data <- bind_rows(
-  mutate(fr10_data, Dataset = "fr10"),
-  mutate(drp10_data, Dataset = "drp10")
+  mutate(fr10_data, Query = "fr10"),
+  mutate(drp10_data, Query = "drp10")
 )
 
-# Extract unique species from the tree file
-tree_file_path <- "searched_species.treefile"
-tree <- read.tree(tree_file_path)
-all_species <- tree$tip.label
+
+# Read species order
+tree_order <- read.table("plots/tree_order.txt", header = FALSE, col.names = "Species")
+all_species <- tree_order$Species
 
 # Create a data frame with all species and their ranges
 all_data <- data.frame(
   Species = rep(all_species, 2),
   Start = -1, # Replace with a value that won't be plotted
   End = -1,   # Replace with a value that won't be plotted
-  Dataset = rep(c("fr10", "drp10"), each = length(all_species))
+  Query = rep(c("fr10", "drp10"), each = length(all_species))
 )
 
 range_data <- bind_rows(all_data, combined_data)
 
 # Ensure Dataset is a factor with correct levels
-range_data$Dataset <- factor(range_data$Dataset, levels = c("drp10", "fr10"))
+range_data$Query <- factor(range_data$Query, levels = c("drp10", "fr10"))
+
+# Load and add overlap data
+overlap_data <- read.csv("plots/coverage_overlap.csv", header = FALSE, col.names = c("Species", "Start", "End", "Query", "ID" ))
+range_data <- bind_rows(range_data, overlap_data)
 
 # Plot using ggplot
-ggplot(range_data, aes(xmin = Start, xmax = End, ymin = Species, ymax = Species, fill = Dataset, color = Dataset)) +
+ggplot(range_data, aes(xmin = Start, xmax = End, ymin = Species, ymax = Species, fill = Query, color = Query)) +
   geom_rect(linewidth = 1) +
-  scale_y_discrete(limits = rev(all_species), name = "Species Searched") +
-  labs(x = "Range", title = "Query Coverage Comparison") +
+  scale_y_discrete(limits = all_species) +
+  labs(x = "Query Range", title = "Query Coverage") +
   theme_minimal() +
-  scale_fill_manual(values = c("drp10" = "orange", "fr10" = "skyblue")) +
-  scale_color_manual(values = c("drp10" = "orange", "fr10" = "skyblue"))
+  scale_fill_manual(values = c("drp10" = "brown1", "fr10" = "deepskyblue", "overlap" = "darkorchid")) +
+  scale_color_manual(values = c("drp10" = "brown1", "fr10" = "deepskyblue", "overlap" = "darkorchid"))
